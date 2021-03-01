f07c8034e94a7a936e77ed41af869e331c48c910
Patrick Ramirez <patrickramirez5087@gmail.com>
2021-02-28 23:55:15 -0800

    Setup Xcode Project

---
+++ CanfieldCodingChallenge/CanfieldCodingChallenge/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  CanfieldCodingChallenge
+//
+//  Created by Patrick Ramirez on 2/28/21.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
---
+++ CanfieldCodingChallenge/CanfieldCodingChallenge/UI Layer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CanfieldCodingChallenge
+//
+//  Created by Patrick Ramirez on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
